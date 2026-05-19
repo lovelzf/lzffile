f24e11e70d6326d6dd0e3de7d9509c6b2ab0e0d1
lovelzf <74540192+lovelzf@users.noreply.github.com>
2026-05-19 10:09:41 +0800

    11

---
+++ config/dictionary_update.sql
@@ -0,0 +1,20 @@
+-- 词条更新SQL语句
+-- 生成时间: 2026-05-19 09:42:52
+-- 共 16 条更新语句
+
+UPDATE `t_sys_dictionary` SET `target` = 'Monitoring Point' WHERE sourceLanguage='ZH_CN' AND targetLanguage='EN_US' AND `source` = '测点';
+UPDATE `t_sys_dictionary` SET `target` = 'Wire Breaks' WHERE sourceLanguage='ZH_CN' AND targetLanguage='EN_US' AND `source` = '断丝';
+UPDATE `t_sys_dictionary` SET `target` = 'Statistic Analysis' WHERE sourceLanguage='ZH_CN' AND targetLanguage='EN_US' AND `source` = '统计分析';
+UPDATE `t_sys_dictionary` SET `target` = 'Mechanism' WHERE sourceLanguage='ZH_CN' AND targetLanguage='EN_US' AND `source` = '机构';
+UPDATE `t_sys_dictionary` SET `target` = 'Hoist' WHERE sourceLanguage='ZH_CN' AND targetLanguage='EN_US' AND `source` = '起升';
+UPDATE `t_sys_dictionary` SET `target` = 'Left Outer' WHERE sourceLanguage='ZH_CN' AND targetLanguage='EN_US' AND `source` = '左外';
+UPDATE `t_sys_dictionary` SET `target` = 'Catenary Trolley' WHERE sourceLanguage='ZH_CN' AND targetLanguage='EN_US' AND `source` = '托架小车';
+UPDATE `t_sys_dictionary` SET `target` = 'Right Outer' WHERE sourceLanguage='ZH_CN' AND targetLanguage='EN_US' AND `source` = '右外';
+UPDATE `t_sys_dictionary` SET `target` = 'Defects' WHERE sourceLanguage='ZH_CN' AND targetLanguage='EN_US' AND `source` = '缺陷';
+UPDATE `t_sys_dictionary` SET `target` = 'Number of Defects' WHERE sourceLanguage='ZH_CN' AND targetLanguage='EN_US' AND `source` = '缺陷数量';
+UPDATE `t_sys_dictionary` SET `target` = 'Critical' WHERE sourceLanguage='ZH_CN' AND targetLanguage='EN_US' AND `source` = '危险';
+UPDATE `t_sys_dictionary` SET `target` = 'Dual-hoist QC' WHERE sourceLanguage='ZH_CN' AND targetLanguage='EN_US' AND `source` = '双起升岸桥';
+UPDATE `t_sys_dictionary` SET `target` = 'Wire Protrusion' WHERE sourceLanguage='ZH_CN' AND targetLanguage='EN_US' AND `source` = '钢丝突出';
+UPDATE `t_sys_dictionary` SET `target` = 'Bulk Delete' WHERE sourceLanguage='ZH_CN' AND targetLanguage='EN_US' AND `source` = '批量删除';
+UPDATE `t_sys_dictionary` SET `target` = 'Disabled' WHERE sourceLanguage='ZH_CN' AND targetLanguage='EN_US' AND `source` = '未启用';
+UPDATE `t_sys_dictionary` SET `target` = 'Equipment List' WHERE sourceLanguage='ZH_CN' AND targetLanguage='EN_US' AND `source` = '设备列表';
